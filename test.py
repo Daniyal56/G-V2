de8ee93247cd3f4f4cf006449b0c1b535e8cf8f1
Daniyal Alam <49856315+Daniyal56@users.noreply.github.com>
2020-10-26 09:51:03 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os,time
-
-CLIENT_FOLDER = 'C:/Users/Danyal/Desktop/Garibsons Setup for server/Garibson Web App/'
-file_list = os.listdir(CLIENT_FOLDER)
-dic = {key: time.ctime(os.path.getmtime(
-    os.path.join(CLIENT_FOLDER, key))) for key in file_list}
-
-print(dic)
